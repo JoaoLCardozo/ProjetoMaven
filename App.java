d97fc49440b6f0a0db3cbf001c2ddf931dffec27
João Luiz Ferreira Cardozo <146985799+JoaoLCardozo@users.noreply.github.com>
2024-10-02 18:11:25 -0300

    App.java

---
+++ App.java
@@ -0,0 +1,12 @@
+package com.exemplo;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Olá, Jenkins! Este é um pipeline de build para Java.");
+    }
+
+    // Método simples para demonstração de teste
+    public int somar(int a, int b) {
+        return a + b;
+    }
+}
